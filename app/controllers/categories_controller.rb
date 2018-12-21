2599a77214dd6cf2691c4842cd53b2ff780a0a63
Chaitanya Baranwal <chaitanyabaranwal@gmail.com>
2018-12-22 00:06:01 +0530

    Add new category modal

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -25,16 +25,18 @@ class CategoriesController < ApplicationController
   # POST /categories.json
   def create
     @category = Category.new(category_params)
+    @categories = Category.all
+    @category.save
 
-    respond_to do |format|
-      if @category.save
-        format.html { redirect_to @category, notice: 'Category was successfully created.' }
-        format.json { render :show, status: :created, location: @category }
-      else
-        format.html { render :new }
-        format.json { render json: @category.errors, status: :unprocessable_entity }
-      end
-    end
+    # respond_to do |format|
+    #   if @category.save
+    #     format.html { redirect_to categories_path, notice: 'Category was successfully created.' }
+    #     format.json { render :show, status: :created, location: @category }
+    #   else
+    #     format.html { render :new }
+    #     format.json { render json: @category.errors, status: :unprocessable_entity }
+    #   end
+    # end
   end
 
   # PATCH/PUT /categories/1
--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -27,17 +27,19 @@ class TasksController < ApplicationController
   # POST /tasks.json
   def create
     @task = Task.new(task_params)
+    @tasks = Task.all
     @task.category_id = params[:category_id]
+    @task.save
 
-    respond_to do |format|
-      if @task.save
-        format.html { redirect_to @task, notice: 'Task was successfully created.' }
-        format.json { render :show, status: :created, location: @task }
-      else
-        format.html { render :new }
-        format.json { render json: @task.errors, status: :unprocessable_entity }
-      end
-    end
+    # respond_to do |format|
+    #   if @task.save
+    #     format.html { redirect_to tasks_path, notice: 'Task was successfully created.' }
+    #     format.json { render :show, status: :created, location: @task }
+    #   else
+    #     format.html { render :new }
+    #     format.json { render json: @task.errors, status: :unprocessable_entity }
+    #   end
+    # end
   end
 
   # PATCH/PUT /tasks/1

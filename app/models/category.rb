8c404322afbcf2029961bf249e49d01725070bc2
Chaitanya Baranwal <chaitanyabaranwal@gmail.com>
2018-12-23 13:18:00 +0530

    Add model validations

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,3 +1,6 @@
 class Category < ApplicationRecord
     has_many :tasks
+
+    validates :name, presence: true, length: {minimum: 5}
+    validates :description, presence: true, length: {minimum: 10}
 end
--- app/models/task.rb
+++ app/models/task.rb
@@ -1,3 +1,15 @@
 class Task < ApplicationRecord
     belongs_to :category
+
+    validates :name, presence: true, length: { minimum: 5 }
+    validates :category_id, presence: true
+    validates :completed, presence: true
+    validate :due_date_in_future
+
+    private
+    def due_date_in_future
+        if !due_date.blank? && due_date < Date.today
+            self.errors.add(:due_date, "Due date cannot be in the past")
+        end
+    end
 end

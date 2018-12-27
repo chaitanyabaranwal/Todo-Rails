6eda0de9bace29bf560bf08d5489e97405ddd803
Chaitanya Baranwal <chaitanyabaranwal@gmail.com>
2018-12-27 12:54:20 +0530

    Add seed data to database

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,22 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+user1 = User.create(username: 'Nicole', email: 'nicole@gmail.com', password: 'password1234', password_confirmation: 'password1234')
+user2 = User.create(username: 'Bruce', email: 'bruce@gmail.com', password: 'password1234', password_confirmation: 'password1234')
+
+1.upto(2) do |i|
+	Category.create(name: "category #{i}", description: "test_category", user: user1)
+end
+
+3.upto(4) do |i|
+	Category.create(name: "category #{i}", description: "test_category", user: user2)
+end
+
+1.upto(2) do |i|
+	Task.create(name: "Task #{i}", user: user1, category_id: i)
+end
+
+3.upto(4) do |i|
+	Task.create(name: "Task #{i}", user: user2, category_id: i)
+end

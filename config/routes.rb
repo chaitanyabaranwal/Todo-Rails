d188f52950a5f9c0877cb41755b3dd310fa11c79
Chaitanya Baranwal <chaitanyabaranwal@gmail.com>
2018-12-28 10:37:47 +0530

    Display tasks in category table

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
-  resources :categories
-  resources :tasks
+  resources :categories, :except => [:show]
+  resources :tasks, :except => [:show]
   
   root 'tasks#index'
 end

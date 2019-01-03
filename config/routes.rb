57caff2e8cae9a2d0278b6ca44c0b4a6c1af89c1
Chaitanya Baranwal <chaitanyabaranwal@gmail.com>
2019-01-03 12:16:45 +0530

    Add navbar dropdown and sessions

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,12 @@
 Rails.application.routes.draw do
+  root 'tasks#index'
+
   resources :users
+  resources :sessions, only: [:new, :create, :destroy]
   resources :categories, :except => [:show]
   resources :tasks, :except => [:show]
-  
-  root 'tasks#index'
+
+  get 'signup', to: 'users#new', as: 'signup'
+  get 'login', to: 'sessions#new', as: 'login'
+  get 'logout', to: 'sessions#destroy', as: 'logout'
 end
